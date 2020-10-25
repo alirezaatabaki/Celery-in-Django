7f20aef40eaf8bb8053430bad5faed9d00d9e0cb
alirezaatabaki <alirezaatabaki@icloud.com>
2020-10-25 10:48:52 +0330

    running tasks

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'home'
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('<int:num_id>/', views.nums, name='nums')
 ]
